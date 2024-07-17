7ad1d928206d3af841c7efb75e233df2ad99bca7
Simon Queric <p120616@romane>
2024-07-17 10:00:25 -0400

    change getitem so that it returns a dictionary

--- dataset.py
+++ dataset.py
@@ -1,36 +1,10 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import nibabel as nib
-import pydicom as dicom
-import cv2
 from tqdm import tqdm
-import os
 import glob
-import time
-import sys
-from monai.data import Dataset, DataLoader, CacheDataset
-from monai.transforms import (
-    Compose,
-    RandFlip,
-    RandRotate90,
-    RandRotate,
-    RandShiftIntensity,
-    ToTensor,
-    RandSpatialCrop,
-    LoadImage,
-    SqueezeDim,
-    RandRotate,
-    RandSimulateLowResolution,
-    Resize,
-    CenterSpatialCrop,
-)
 import torch
-from nilearn.plotting import view_img
+from monai.data import Dataset
 
-sys.path.append(os.path.abspath(os.path.join("code")))
-from image import Image
-from sklearn.model_selection import train_test_split
+
 
 class RSNADataset(Dataset):
     """
@@ -48,11 +22,12 @@ class RSNADataset(Dataset):
     """
     def __init__(self, root_dir : str, study_ids : list, 
                  seqtype : str, label_df : pd.DataFrame, 
-                 train : bool = True, exclude : list = None):
-        
-        
+                 train : bool = True, exclude : list = None,
+                 transform : any = None):
+                
         orientation, contrast = seqtype.split("-")
         
+        self.transform = transform
         self.study_ids = []
         self.images_paths = []
         self.labels = []
@@ -61,9 +36,10 @@ class RSNADataset(Dataset):
         for i, study_id in tqdm(enumerate(study_ids)):
             
             pursuie = True
-            for sub in exclude : 
-                if str(study_id) in sub:
-                    pursuie = False 
+            if exclude is not None:
+                for sub in exclude : 
+                    if str(study_id) in sub:
+                        pursuie = False 
             
             if pursuie:
                 paths = glob.glob(root_dir+"/sub-"+str(study_id)+"/anat/*"+orientation+"*"+contrast+"*.nii.gz")
@@ -102,9 +78,9 @@ class RSNADataset(Dataset):
         return len(self.study_ids)
         
     def __getitem__(self, index):
+        datad = {"image" : None, "label" : None, "study_id" : None}
+        
         id = self.study_ids[index]
-        volume = nib.load(self.images_paths[index]).get_fdata()
-        volume = torch.Tensor(volume)
         label = self.labels[index]
         n = len(label)
         y = torch.zeros((n, 3))
@@ -115,5 +91,13 @@ class RSNADataset(Dataset):
                 y[i] = torch.tensor([0, 1, 0])
             if label[i]==2:
                 y[i] = torch.tensor([0, 0, 1]) 
+    
+        datad["image"] = self.images_paths[index]
+        datad["label"] = y 
+        datad["study_id"] = id
         
-        return volume, y, id
+        if self.transform is not None:
+            return self.transform(datad)
+
+        else:            
+            return datad
