23a3811889fffb193e8316c80e8f364f7601aa6d
Thomas Dagonneau <thdaga@romane>
2024-11-05 17:43:11 -0500

    The problem with the niftification evoqued in last commit should be fixed. Warning the complete pipeline has not been tested for now !

--- niftification.py
+++ niftification.py
@@ -1,8 +1,3 @@
-## STEP 1 ##
-
-# this is the first part of the preprocessing pipeline
-# it aims to convert the raw data to nifti format and save it with the BIDS convention
-
 import os
 import subprocess
 import dcm2niix
@@ -11,8 +6,8 @@ import shutil
 import nibabel as nib
 import numpy as np
 import sys
-
-
+import pandas as pd
+from tqdm import tqdm
 
 # use a subprocess to convert the dicom images to nifti format, requires the output path
 def convert_dicom_to_nifti(subject_id, series_uid, input_path, output_path):
@@ -34,11 +29,9 @@ def convert_dicom_to_nifti(subject_id, series_uid, input_path, output_path):
     
     try:
         subprocess.run(dcm2niix_command, shell=True, check=True)
-        
     except subprocess.CalledProcessError as e:
         print(f"Error converting {input_file}: {e}")
         return None
-
 
 # do not apply this function to the axial acquisitions, as it will merge different acquisitions with different orientations
 def merge_nifti_volumes(output_path, subject_id, series_uid):
@@ -61,19 +54,17 @@ def merge_nifti_volumes(output_path, subject_id, series_uid):
     if len(filenames) > 1:
         for filename in filenames: 
             merged_filename = f"sub-{subject_id}_run-{series_uid}_{filename[-14:]}"
-            merged_path = os.path.join(output_folder, merged_filename)  # Changed to output_folder
+            merged_path = os.path.join(output_path, merged_filename)  # Changed to output_folder
             os.rename(filename, merged_path)
             new_paths.append(merged_path)
     elif len(filenames) == 1:
         merged_filename = f"sub-{subject_id}_run-{series_uid}.nii.gz"
-        merged_path = os.path.join(output_folder, merged_filename)  # Changed to output_folder
+        merged_path = os.path.join(output_path, merged_filename)  # Changed to output_folder
         os.rename(filenames[0], merged_path)
         new_paths.append(merged_path)
     return new_paths
 
-        
-
-# global function that process one subject creating nifti volumes in the bids format
+# global function that processes one subject creating nifti volumes in the bids format
 def process_subject(subject_id, input_path, output_path, train, meta_obj):
     """
     Process DICOM to NIfTI conversion, merge volumes, and save with corrected orientation if applicable.
@@ -95,8 +86,8 @@ def process_subject(subject_id, input_path, output_path, train, meta_obj):
         return
 
     # create output directories if not existing
-    os.makedirs(os.path.join(output_folder, f'sub-{subject_id}'), exist_ok=True)
-    os.makedirs(os.path.join(output_folder, f'sub-{subject_id}', 'anat'), exist_ok=True)
+    os.makedirs(os.path.join(output_path, f'sub-{subject_id}'), exist_ok=True)
+    os.makedirs(os.path.join(output_path, f'sub-{subject_id}', 'anat'), exist_ok=True)
 
     # process through each acquisition of the subject    
     for idx, series_uid in enumerate(ptobj['SeriesInstanceUIDs']):
@@ -116,7 +107,7 @@ def process_subject(subject_id, input_path, output_path, train, meta_obj):
         else:
             continue
 
-        corrected_nifti_path = os.path.join(output_folder, f"sub-{subject_id}/anat/sub-{subject_id}_acq-{acq}_rec{series_uid}_{modality}")
+        corrected_nifti_path = os.path.join(output_path, f"sub-{subject_id}/anat/sub-{subject_id}_acq-{acq}_rec{series_uid}_{modality}")
         if len(new_paths) > 1 : 
             for merged_nifti_path in new_paths : 
                 anat_img = nib.load(merged_nifti_path)
@@ -134,7 +125,6 @@ def process_subject(subject_id, input_path, output_path, train, meta_obj):
 
                 nib.save(nib.Nifti1Image(anat_data, new_affine, header=anat_header), new_path)
 
-                
         else : 
             for merged_nifti_path in new_paths : 
                 anat_img = nib.load(merged_nifti_path)
@@ -148,21 +138,61 @@ def process_subject(subject_id, input_path, output_path, train, meta_obj):
 
                 nib.save(nib.Nifti1Image(anat_data, new_affine, header=anat_header), corrected_nifti_path + '.nii.gz')
 
-# You can add the main function to run the processing
+# Main function to run the processing
 def main():
-    # Check if input folder is provided
-    if len(sys.argv) != 2:
-        print("Usage: python niftification.py [input_folder]")
+    # Check if everything is provided
+    if len(sys.argv) != 3:
+        print("Usage: python niftification.py [input_folder] [csv_description]")
         sys.exit(1)
 
     input_folder = sys.argv[1]
+    csv_description = sys.argv[2]
     output_folder = f"{input_folder}_nii"
     os.makedirs(output_folder, exist_ok=True)
 
-    ### Need to create the dictionnary based on the csv file ### 
+    ### Create the dictionary based on the CSV file ###
+    df_meta_f = pd.read_csv(csv_description)
 
-    for subject_id in subject_ids:  # Define subject_ids appropriately
-        process_subject(subject_id, input_folder, output_folder, train, meta_obj)
+    subject_ids = np.unique(df_meta_f["study_id"].values)
+
+    # List out all of the Studies we have on patients.
+    part_1 = os.listdir(input_folder)
+    part_1 = list(filter(lambda x: x.find('.DS') == -1, part_1))
+
+    p1 = [(x, f"{input_folder}/{x}") for x in part_1]
+    meta_obj = { p[0]: { 'folder_path': p[1], 
+                        'SeriesInstanceUIDs': [] 
+                    } 
+                for p in p1 }
+
+    for m in meta_obj:
+        meta_obj[m]['SeriesInstanceUIDs'] = list(
+            filter(lambda x: x.find('.DS') == -1, 
+                os.listdir(meta_obj[m]['folder_path'])
+                )
+        )
+    # Grabs the corresponding series descriptions
+    for k in tqdm(meta_obj):
+        for s in meta_obj[k]['SeriesInstanceUIDs']:
+            if 'SeriesDescriptions' not in meta_obj[k]:
+                meta_obj[k]['SeriesDescriptions'] = []
+            try:
+                meta_obj[k]['SeriesDescriptions'].append(
+                    df_meta_f[(df_meta_f['study_id'] == int(k)) & 
+                    (df_meta_f['series_id'] == int(s))]['series_description'].iloc[0])
+            except:
+                None
+
+    # Process subjects and set up directories
+    for subject_id in tqdm(subject_ids):  # Adjust range as needed: 1975 subjects
+        subject_id = str(subject_id)
+        
+        # Create specific directories
+        os.makedirs(os.path.join(output_folder, f'sub-{subject_id}'), exist_ok=True)
+        os.makedirs(os.path.join(output_folder, f'sub-{subject_id}', 'anat'), exist_ok=True)
+
+        # Process subject and set up directories
+        process_subject(subject_id, input_folder, output_folder, df_meta_f, meta_obj)
 
 if __name__ == "__main__":
     main()
